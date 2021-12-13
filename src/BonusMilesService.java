930f6cc4fdc64d65d58b7c64fcacf8b6c2493854
Taras Zhurenko <zhta21@yandex.ru>
2021-12-14 00:50:26 +0300

    Create BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,2 +1,6 @@
 public class BonusMilesService {
+    public int calculate(int ticketPrice, int oneBonusMile) {
+        int bonusMiles = ticketPrice / oneBonusMile;
+        return bonusMiles;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 11_111;
-        int oneBonusMile = 20;
-        int bonusMiles = ticketPrice / oneBonusMile;
+        BonusMilesService service = new BonusMilesService();
+        int bonusMiles = service.calculate(11_111,20);
         System.out.println("Будет начислено бонусных миль: " + bonusMiles);
     }
 }
